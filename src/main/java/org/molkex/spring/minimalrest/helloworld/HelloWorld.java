368222d48d3c1cad7d69a96443683e2789c8a3e9
Roman De Meo <rdemeo@nero-networks.de>
2019-06-29 16:02:36 +0200

    Add a tiny but swagger-ui enabled rest controller

--- src/main/java/org/molkex/spring/minimalrest/helloworld/HelloWorld.java
+++ src/main/java/org/molkex/spring/minimalrest/helloworld/HelloWorld.java
@@ -1,11 +1,29 @@
 package org.molkex.spring.minimalrest.helloworld;
 
+import io.swagger.annotations.ApiOperation;
+import org.molkex.spring.minimalrest.MinimalRestApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@RequestMapping("/hello")
 public class HelloWorld {
-    public static void main(String[] args) {
-        SpringApplication.run(HelloWorld.class, args);
+
+    /** This endpoint returns "Hello " + name. */
+    @GetMapping("/{name}")
+    @ApiOperation(value = "This endpoint returns \"Hello \" + name", nickname = "hello")
+    public String hello(@PathVariable String name) {
+        return "Hello " + name;
+    }
+
+    @SpringBootApplication
+    public static class App extends MinimalRestApp {
+        public static void main(String[] args) {
+            SpringApplication.run(App.class, args);
+        }
     }
 }
